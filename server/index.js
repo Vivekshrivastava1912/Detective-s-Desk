8aa78d675c1d5eeb9323ab2c4ee84da371f3b489
Vivekshrivastava1912 <shrivastavavivek19d02005@gmail.com>
2026-03-11 10:11:58 +0530

    mongoDB-database-create-and-connect

--- server/index.js
+++ server/index.js
@@ -1,12 +1,44 @@
-const express = require('express');
-const app = express();
-const PORT = 8000;
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import connectDB from './config/connectDB.js'
 
-// Ek test route
-app.get('/', (req, res) => {
-  res.send('Hello! Mera backend server start ho gaya hai!');
-});
- 
-app.listen(PORT, () => {
-  console.log(`Server is running :- ${PORT}`);
-});
+dotenv.config()
+
+const app = express()
+
+app.use(cors({
+    origin: "http://localhost:5173", 
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"] 
+}));
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(morgan('dev')) 
+app.use(helmet({
+    crossOriginResourcePolicy: false
+}))
+
+app.get('/', (request, response) => {
+    response.json({
+        message: "Server is running vivek " + (process.env.PORT || 8000)
+    })
+})
+
+const PORT = process.env.PORT || 8000
+
+// Yahan se humne // hata diya hai taaki server chalu ho jaye
+
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`)
+    })
+}).catch((error) => {
+    console.log('Failed to connect to database', error)
+})
